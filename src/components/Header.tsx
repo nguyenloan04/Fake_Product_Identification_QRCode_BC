816df7af1d7d76ecf1073452e56fbf47b65652c6
tandang <21130171@st.hcmuaf.edu.vn>
2025-03-03 15:07:55 +0700

    update product detail

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,12 @@ const Header = () => {
           <Icon color="#797979" icon="mdi:linkedin" width="24" height="24" />
         </div>
         <div className="flex items-center gap-4">
+          <div>
+            <button className="px-4 py-2 bg-[#2D5F4D] text-white rounded-md hover:bg-green-600">
+              Đăng nhập
+            </button>
+          </div>
+          <Icon icon="mdi:cart" width="24" height="24" />
           <Icon color="#2D5F4D" icon="mdi:telephone" width="24" height="24" />
           <span className="text-[#2D5F4D] font-bold">9430144469</span>
         </div>
